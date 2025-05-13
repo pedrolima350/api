cb8b397cec3aad68d2fc54c03d5d86735780aad2
Morning_Coffee <blackghost3241@gmail.com>
2025-05-13 16:31:27 -0300

    Fetch por ID brabissimo

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
-const route = "characters";
+const route = "characters/";
 const host = "https://dattebayo-api.onrender.com/";
+var id = [];
 
 fetch(`${host}${route}`, {
     method: "GET",
@@ -20,26 +21,50 @@ fetch(`${host}${route}`, {
 
     // console.log(data.characters[0].jutsu);
 
-    var naruto = [];
-    naruto = data.characters[0].jutsu;
+    // var naruto = [];
+    // naruto = data.characters[0].jutsu;
 
-    var id = [];
+   // var id = [];
 
     for(let i = 0; i < data.characters.length; i++){
         id[i] = data.characters[i].id;
     }
 
+    return id;
     
-    for (let i = 0; i < id.length; i++){
-        console.log("ID:",id[i],"Nome: ", data.characters[i].name);
-    }
+   // for (let i = 0; i < id.length; i++){
+   //     console.log("ID:",id[i],"Nome: ", data.characters[i].name);
+   // }
 
 
     // for (let i = 0; i < naruto.length; i++){
     //     console.log("Jutsu ", i,":",naruto[i].toUpperCase());
     // }
 
+}).then((id) => {
+
+for (let i = 0; i < id.length; i++){
+    fetch(`${host}${route}${id[i]}`, {
+        method: "GET",
+        headers :{
+            Accept: 'application/json'
+        } 
+
+    }).then((Persons) =>{
+        return Persons.json();
+
+    }).then((InfoCharacters) => {
+        console.log("======================================================")
+        console.log("O personagem de ID: ", id[i])
+        console.log(InfoCharacters);
+    })
+}
+
+
+
 })
+
+
 .catch((error) => {
     console.log(error);
 
